6dd26084348bbb361f11573899cc70db4b94b65c
Sidharth CR <sidhusidharth412@gmail.com>
2025-09-29 10:58:01 +0530

    commit

---
+++ Day 12/filter.js
@@ -0,0 +1,5 @@
+let numArray = [5, 8, 7, 6, 10, 42, 40];
+
+let evenArray=numArray.filter((eachElement)=>eachElement%2==0)
+
+console.log(evenArray)
---
+++ Day 12/find.js
@@ -0,0 +1,4 @@
+let numArray = [5, 8, 7, 6, 10, 42, 40];
+
+let firstEven = numArray.find((eachElement) => eachElement % 2 == 0)
+console.log(firstEven)
